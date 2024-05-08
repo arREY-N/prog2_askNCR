7c8915467856116cab0ffad11cd5e5c4893c6818
Spectre <134749370+Spectre1031@users.noreply.github.com>
2024-05-08 16:13:11 +0800

    modify Validation.java

--- src/Validation.java
+++ src/Validation.java
@@ -1,3 +1,45 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Validation {
+    public static boolean isAlphanumeric(String username,String password) throws InvalidInputException{ 
+        if (input != null && input.matches("[a-zA-Z0-9]+")){
+            return true;
+        } else {
+            throw new InvalidInputException();
+        }
+    }
     
+    public static void login(String username, String password) {
+        if (!getCredentials().containsKey(username)) {
+            System.out.println("User not found.");
+        } else {
+            if (!getCredentials().get(username).equals(password)) {
+                System.out.println("Incorrect password.");
+            } else {
+                System.out.println("Login Successful.");
+            }
+        }      
+    }
+
+    public static void signin(String username, String password) {
+        if (!isAlphanumeric(username) || !isAlphanumeric(password)) {
+            System.out.println("Username and password must be alphanumeric.");
+        }
+        else{
+            if (getCredentials().containsKey(username)) {
+                System.out.println("User already exists.");
+            }
+        }
+        else{
+            getCredentials().put(username, password);
+            saveCredentialsToFile(credentials);
+            System.out.println("User [" + username + "] Signed-In successfully.");
+        } 
+    }
 }
+
+//if(Database.getAccounts().containsKey(username)){
+//
+//}
